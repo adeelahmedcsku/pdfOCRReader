1027e1699deb051800ca64fcf83f4ec859d9daaf
14jchan <14jchan@gmail.com>
2022-04-08 01:38:30 -0700

    Update FilesController.java

--- src/main/java/com/chansoft/Controllers/FilesController.java
+++ src/main/java/com/chansoft/Controllers/FilesController.java
@@ -1,15 +1,16 @@
-package com.example.demo.Controllers;
+package com.chansoft.Controllers;
 
 import java.io.File;
 import java.util.UUID;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Services.FilesServices;
+import com.chansoft.Services.FilesServices;
 
 @RestController
 public class FilesController {
@@ -17,7 +18,7 @@ public class FilesController {
 	@Autowired
 	FilesServices filesServices;
 
-	@RequestMapping("/indexItems")
+	@RequestMapping(path = "/indexItems", method = RequestMethod.GET)
 	public String updateItem(@RequestParam(value = "base64Directory") String directory) {
 		if (directory.isBlank()) {
 			return "Please enter a directory in base64 format";
@@ -34,7 +35,7 @@ public class FilesController {
 		}
 	}
 
-	@RequestMapping("/lol")
+	@RequestMapping(path = "/lol", method = RequestMethod.GET)
 	public String returnLOL() {
 		return "LOL";
 	}
